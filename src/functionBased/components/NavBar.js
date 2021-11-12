ffff4a6a42d8571b92898012fb815e2b6589ee9f
alexander16108 <alexandermayowa05@gmail.com>
2021-11-12 10:53:48 +0100

    Adding Routing for a Single page app with nav etc

--- src/functionBased/components/NavBar.js
+++ src/functionBased/components/NavBar.js
@@ -1,5 +1,8 @@
-import React from "react"
-import { NavLink } from "react-router-dom"
+import React, { useState } from "react";
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
+import './App.css';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const links = [
@@ -15,13 +18,33 @@ const Navbar = () => {
     },
   ]
 
+  const [navbarOpen, setNavbarOpen] = useState(false)
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen)
+  }
+
+  const closeMenu = () => {
+    setNavbarOpen(false)
+  }
+
   return (
     <nav className="navBar">
-      <ul>
+<button onClick={handleToggle}>
+  {navbarOpen ? (
+    <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+  ) : (
+    <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+  )}
+</button>
+      <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
         {links.map(link => {
 return (
   <li key={link.id}>
-    <NavLink to={link.path} activeClassName="active-link" exact>
+    <NavLink
+     to={link.path}
+      activeClassName="active-link"
+      onClick={() => closeMenu()} exact>
       {link.text}
     </NavLink>
   </li>
